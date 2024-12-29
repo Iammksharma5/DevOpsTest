8117d026a02c60f38715ecc1336c77e046d606a6
AWS -  Focus on being productive instead of busy <77732142+Iammksharma5@users.noreply.github.com>
2024-12-29 18:51:41 +0530

    Update dockerimage.sh

--- dockerimage.sh
+++ dockerimage.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Define variables
-IMAGE_NAME="my-python-app"
+IMAGE_NAME="nginx-web-app"
 TAG="latest"
 
 # Build the Docker image
 echo "Building Docker image..."
 docker build -t $IMAGE_NAME:$TAG .
 
-# Optionally, run the Docker container
+# Run the Docker container
 echo "Running Docker container..."
 docker run -d -p 80:80 $IMAGE_NAME:$TAG
